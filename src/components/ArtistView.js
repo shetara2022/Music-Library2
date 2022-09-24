21091b913c40898dfad980f4d9f2736dae1cefb4
shetara2022 <smithshetarall@gmail.com>
2022-09-24 09:53:43 -0400

    refactored artist and album view to include data

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,16 +1,46 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about our artist
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
-function ArtistView() {
+const ArtistView = () => {
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const allAlbums = artistData.filter(entity => entity.collectionType === 'Album')
+        .map((album, i) => {
+            return (
+                <div key={i}>
+                    <Link to={`/album/${album.collectionId}`}>
+                        <p>{album.collectionName}</p>
+                    </Link>
+                </div>)
+        })
+
+    const navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => { navigate(-1) }}>Home</button>
+                |
+                <button onClick={() => navigate('/')}>Back</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <p>Loading...</p>}
+            {navButtons()}
+            {allAlbums}
         </div>
     )
 }
